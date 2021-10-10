7cc268e113289c67093a17e5f05ec79c747312f3
nRookie <228611676@qq.com>
2021-10-10 20:39:20 +0800

    add constructor and setter injection example

---
+++ src/main/java/io/datajek/springbasics/movierecommmendersystem/lesson7/CollaborativeFilter.java
@@ -0,0 +1,14 @@
+package io.datajek.springbasics.movierecommmendersystem.lesson7;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CollaborativeFilter implements Filter{
+	 
+    public String[] getRecommendations(String movie) {
+       //logic of collaborative filter
+    	return new String[] {"Finding Nemo", "Ice Age", "Toy Story"};
+    }
+}
---
+++ src/main/java/io/datajek/springbasics/movierecommmendersystem/lesson7/Filter.java
@@ -0,0 +1,5 @@
+package io.datajek.springbasics.movierecommmendersystem.lesson7;
+
+public interface Filter {
+	  public String[] getRecommendations(String movie);
+}
