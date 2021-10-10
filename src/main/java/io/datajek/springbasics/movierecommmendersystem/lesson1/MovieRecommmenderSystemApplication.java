c875bee3727498ac1abcedc1a066e75336dab25b
nRookie <228611676@qq.com>
2021-10-10 19:43:40 +0800

    init commit

---
+++ src/main/java/io/datajek/springbasics/movierecommmendersystem/lesson1/MovieRecommmenderSystemApplication.java
@@ -0,0 +1,16 @@
+package io.datajek.springbasics.movierecommmendersystem.lesson1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Arrays;
+@SpringBootApplication
+public class MovieRecommmenderSystemApplication {
+
+	public static void main(String[] args) {
+        RecommenderImplementation recommender = new 
+RecommenderImplementation();    
+        String[] result = recommender.recommendMovies("Finding Dory");
+        System.out.println(Arrays.toString(result));
+	}
+
+}
