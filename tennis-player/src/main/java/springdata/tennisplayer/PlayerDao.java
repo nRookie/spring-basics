a4e93846d1ebef69783da6f886353972619cdb09
nRookie <228611676@qq.com>
2021-10-17 14:30:51 +0800

    feat: add dao to implement select query

---
+++ tennis-player/src/main/java/springdata/tennisplayer/PlayerDao.java
@@ -0,0 +1,33 @@
+package springdata.tennisplayer;
+
+import java.util.List;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
+
+
+import org.springframework.stereotype.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public class PlayerDao {
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
+	public List<Player> getAllPlayers() {
+		String sql = "SELECT * FROM PLAYER";
+		return jdbcTemplate.query(sql, new BeanPropertyRowMapper<Player>(Player.class));
+	}
+	
+	public Player getPlayerById(int id) {
+		String sql = "SELECT * FROM PLAYER WHERE ID = ?";
+		return jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<Player>(Player.class), new Object[] {id});
+	}
+	
+}
