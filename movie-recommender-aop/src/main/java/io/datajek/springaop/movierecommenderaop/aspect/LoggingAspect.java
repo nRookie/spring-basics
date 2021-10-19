f679af576c5c817c62ab7f1dd427233724f42e2a
nRookie <228611676@qq.com>
2021-10-19 23:44:36 +0800

    show error

---
+++ movie-recommender-aop/src/main/java/io/datajek/springaop/movierecommenderaop/aspect/LoggingAspect.java
@@ -0,0 +1,32 @@
+package io.datajek.springaop.movierecommenderaop.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Aspect
+@Configuration
+public class LoggingAspect {
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@AfterReturning(value = "execution(* io.datajek.springaop.movierecommenderaop.business.*.*(..)", returning="result")
+	public void logAfterExecution(JoinPoint joinPoint, Object result) {
+		logger.info("{} returned with: {}", joinPoint, result);
+	}
+	
+	@AfterThrowing(value = "execution(* io.datajek.springaop.movierecommenderaop.business.*.*(..))", throwing="exception")
+	public void logAfterException(JoinPoint joinPoint, Object exception) {
+			logger.info("Exception in {} returned with: {}", joinPoint, exception);
+	}  
+	
+	@After(value="execution(* io.datajek.springaop.movierecommenderaop.business.*.*(..))")
+	public void logAfterMethod(JoinPoint joinPoint) {
+		logger.info("After {}", joinPoint);
+	}
+}
